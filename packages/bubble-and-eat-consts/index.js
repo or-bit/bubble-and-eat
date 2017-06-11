66ad7f1abaf09ce743140be142c15d3630cfe456
niktekusho <nicola.dalmaso@outlook.com>
2017-06-11 10:14:40 +0200

    Fixed bubble-and-eat-consts linting

--- packages/bubble-and-eat-consts/index.js
+++ packages/bubble-and-eat-consts/index.js
@@ -1,8 +1,22 @@
 // required to be able to use the .env file inside this package
-require('dotenv').config({path: `${process.cwd()}/../bubble-and-eat-consts/.env`});
+require('dotenv').config(
+    {
+        path: `${process.cwd()}/../bubble-and-eat-consts/.env`,
+    }
+);
 
 module.exports = {
-  getServerPort: () => process.env.SERVER_PORT,
-  getServerURL: () => `${process.env.SERVER_PROTOCOL}://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`,
-  getDBURL: () => `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    getServerPort: () => process.env.SERVER_PORT,
+    getServerURL: () => {
+        const protocol = process.env.SERVER_PROTOCOL;
+        const host = process.env.SERVER_HOST;
+        const port = process.env.SERVER_PORT;
+        return `${protocol}://${host}:${port}`;
+    },
+    getDBURL: () => {
+        const host = process.env.DB_HOST;
+        const port = process.env.DB_PORT;
+        const name = process.env.DB_NAME;
+        return `mongodb://${host}:${port}/${name}`;
+    },
 };
