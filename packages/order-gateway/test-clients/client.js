b4fa2d5849f1282a539c34ad76709969cc8c6140
aliceblack <blackfluid44@gmail.com>
2017-06-24 14:42:56 +0200

    Update client.js

--- packages/order-gateway/test-clients/client.js
+++ packages/order-gateway/test-clients/client.js
@@ -1,42 +1,88 @@
+/**
+ * Client module
+ * @module client
+ */
+/**
+ * @constant
+ * @type{config}
+ */
 const config = require('bubble-and-eat-consts');
+/**
+ * @constant
+ * @type{socketImport}
+ */
 const socketImport = require('socket.io-client');
 
 let socket = socketImport(config.getServerURL());
 let orderId = null;
 
+/**
+ * @function
+ * @name soket/on
+ * @param {Event} disconnect - client disconnect event
+ * @param {Function} client_disconnect - client disconnection
+ * @desc Menage client disconnection and eventually client reconnection to server and
+ * recovery state of his order if are any active
+ */
 // TODO: da fare piu pulita la riconnessione
 socket.on('disconnect', () => {
   socket.close();
   console.log('disconnected');
-  // riconnetto
+  // reconnection
   socket = socketImport(config.getServerURL());
-  // riautentico
+  // authentication
   socket.emit('auth', { type: 'client' });
-  // chiedo come e' messo l'ordine
+  // check order status
   socket.emit('orderStatus', orderId);
-  // ne aspetto la risposta
+  // wait for response
   socket.on('orderReady', () => {
     console.log('your order is ready!');
   });
 });
 
-socket.on('menu', (menu) => { console.log('il menu del giorno è', menu); });
+/**
+ * @function
+ * @name soket/on
+ * @param {Event} menu - menu request event
+ * @param {Function} get_request - get menu
+ * @desc return menu to client
+ */
+socket.on('menu', (menu) => { console.log('the menu of the day is', menu); });
 
+/**
+ * @function
+ * @name clientOperations
+ * @desc auth menu order
+ */
 function clientOperations() {
   socket.emit('auth', { type: 'client' });
   socket.emit('menuRequest');
   socket.emit('order', {
     dishes: ['cotoletta alla pera',
-      'insalata veggie',
-      'tofu con crema di banana'],
+      'salad',
+      'tofu and banana cream'],
   });
 }
 clientOperations();
 
+/**
+ * @function
+ * @name soket/on
+ * @param {Event} orderId - order id event
+ * @param {Function} set_id - set id
+ * @desc set id of client's order
+ */
 socket.on('orderId', (id) => {
   orderId = id;
   console.log('your order id is:', id);
 });
+
+/**
+ * @function
+ * @name soket/on
+ * @param {Event} orderReady - order ready event
+ * @desc notify when order is ready
+ */
 socket.on('orderReady', () => {
   console.log('your order is ready!');
 });
